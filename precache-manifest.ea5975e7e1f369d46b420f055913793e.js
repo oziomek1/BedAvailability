75ca6e548a071ab3b7659c94dd600eb1011cdaae
oziomek1 <ozimekwojciech@zoho.eu>
2020-04-04 10:12:08 +0200

    Updates

---
+++ precache-manifest.ea5975e7e1f369d46b420f055913793e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf1d5a0acf537626dbb52533c008bb1",
+    "url": "/BedAvailability/index.html"
+  },
+  {
+    "revision": "c971a547fb6c4b4b98e2",
+    "url": "/BedAvailability/static/css/main.83526b96.chunk.css"
+  },
+  {
+    "revision": "26d93b83264675051605",
+    "url": "/BedAvailability/static/js/2.654b75fd.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/BedAvailability/static/js/2.654b75fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c971a547fb6c4b4b98e2",
+    "url": "/BedAvailability/static/js/main.897d07bf.chunk.js"
+  },
+  {
+    "revision": "6586cdcf8220ce551af4",
+    "url": "/BedAvailability/static/js/runtime-main.f6efc540.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BedAvailability/precache-manifest.a7625e78604ca58f645fd47977ed6fa0.js"
+  "/BedAvailability/precache-manifest.ea5975e7e1f369d46b420f055913793e.js"
 );
 
 self.addEventListener('message', (event) => {
