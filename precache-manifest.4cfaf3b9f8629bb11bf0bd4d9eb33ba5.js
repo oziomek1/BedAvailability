a1036a6c0f382843ffadd088c1774a7a5f787c7a
oziomek1 <ozimekwojciech@zoho.eu>
2020-04-04 09:48:21 +0200

    Updates

---
+++ precache-manifest.4cfaf3b9f8629bb11bf0bd4d9eb33ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca032399e6d555ecd76109bdda72d7dd",
+    "url": "/BedAvailability/index.html"
+  },
+  {
+    "revision": "83c74a1816f55b3c2353",
+    "url": "/BedAvailability/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49a32c8313db3d545df9",
+    "url": "/BedAvailability/static/js/2.50175e33.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/BedAvailability/static/js/2.50175e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c74a1816f55b3c2353",
+    "url": "/BedAvailability/static/js/main.71abcc5f.chunk.js"
+  },
+  {
+    "revision": "6586cdcf8220ce551af4",
+    "url": "/BedAvailability/static/js/runtime-main.f6efc540.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BedAvailability/static/media/logo.5d5d9eef.svg"
+  }
+]);
