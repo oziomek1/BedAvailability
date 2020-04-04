94e1edcb1c2baa678b58db19b4dc3f82090f9a51
oziomek1 <ozimekwojciech@zoho.eu>
2020-04-04 11:13:21 +0200

    add markers

--- src/components/HospitalMap.js
+++ src/components/HospitalMap.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
+import * as hospitalLocations from "../data/hospital-locations.json";
 
 
 export default class HospitalMap extends Component {
     state = {
-        lat: 55.730952,
-        lng: 12.44357,
-        zoom: 9,
+        lat: 55.6761,
+        lng: 12.5683,
+        zoom: 11,
+        activeHospital: null,
+    }
+
+    setActiveHospital(hospital) {
+        this.activeHospital = hospital;
     }
     render() {
         return (
             <Map center={[this.state.lat, this.state.lng]} zoom={this.state.zoom}>
                 <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
+                {hospitalLocations.hospitals.map((hospital, index) => (
+                    <Marker 
+                        key={index} 
+                        position={[hospital.geometry.coordinates.latitude, hospital.geometry.coordinates.longitude]}
+                        onclick={() => {
+                            this.setActiveHospital(hospital);
+                        }}
+                    />
+                ))}
+
+                {this.activeHospital && 
+                    (<Popup
+                        position={[
+                            this.activeHospital.geometry.coordinates.latitude, 
+                            this.activeHospital.geometry.coordinates.longitude
+                        ]}
+                    >
+                        <div>
+                            <h2>{this.activeHospital.name}</h2>
+                            <p>{this.activeHospital.contact.address}</p>
+                        </div>
+                    </Popup>)
+                }
             </Map>
         )
     }
